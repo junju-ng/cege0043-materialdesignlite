6b3b586ee7889533716a6d1ff65f869f1af5e4d1
Junju Ng <ngjunju@outlook.com>
2019-02-07 07:04:09 -0500

    add point/line/circle data and track location automatically

--- ucesjng/www/js/startup.js
+++ ucesjng/www/js/startup.js
@@ -2,3 +2,10 @@ function trackAndCircle(){
 	trackLocation();
 	addPointLinePoly();
 }
+
+// add Point/Line/Circle data and track location automatically - useful for setting up different startup functions!
+function startup(){
+	document.addEventListener('DOMContentLoaded',function(){
+		trackAndCircle();
+	},false);
+}
